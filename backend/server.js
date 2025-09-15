5752baff80b24a8565ef9e0bbee69a395b63c59a
pramod <prmd777@rediffmail.com>
2025-09-15 09:45:17 +0530

    modified secret

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 const client = new Client({
+
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
 });
-
 async function connectDB() {
   try {
     await client.connect();
